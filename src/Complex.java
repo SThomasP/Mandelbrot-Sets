08a5a88d67593fe66d293c1c84c53f85cad83bb5
SThomasP <stp1g15@soton.ac.uk>
2016-02-28 00:32:40 +0000

    extending complex class

--- src/Complex.java
+++ src/Complex.java
@@ -1,6 +1,8 @@
 /**
  * Created by Steffan on 27/02/2016.
  */
+
+import java.math.*;
 public class Complex {
     //Class for storing/getting details of Complex numbers
     protected double realPart;
@@ -16,5 +18,28 @@ public class Complex {
     }
     public Double getImaginary(){
         return imaginaryPart;
+    }
+
+    public Double getMagnitude(){
+        double maggy = Math.sqrt(Math.pow(realPart,2) + Math.pow(imaginaryPart,2));
+        return maggy;
+    }
+
+    public Complex getSquare(){
+        double squaredReal = realPart*realPart - imaginaryPart*imaginaryPart;
+        double squaredImaginary = 2*realPart*imaginaryPart;
+        return new Complex(squaredReal,squaredImaginary);
+    }
+
+    public Double magnitudeSquared(){
+        return Math.pow(getMagnitude(), 2);
+    }
+
+    public Complex add(Complex d){
+        //adds the imaginary and real parts of the two complex numbers, then returns a new complex number with the result
+        double sumReal = d.getReal()+getReal();
+        double sumImaginary = d.getImaginary()+getImaginary();
+        return new Complex(sumReal, sumImaginary);
+
     }
 }
